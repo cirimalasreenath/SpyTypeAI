e654c6c51f5885077f0c3ea372f2f4250aac07b4
cirimalasreenath <sreenathcirimala@gmail.com>
2025-05-04 10:58:33 +0530

    Initial commit

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,6 @@
 class AuthManager:
     def __init__(self):
-        self.admin_password = "admin123"
+        self.admin_password = "Sreenath@123"
 
     def verify(self, password):
         return password == self.admin_password
